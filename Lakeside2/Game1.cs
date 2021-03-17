9a78069017769d38f2a70ea53e5a352fbfdda97b
tapioca <tapioca2k@gmail.com>
2021-03-17 17:56:05 -0400

    resolution scaling

--- Lakeside2/Game1.cs
+++ Lakeside2/Game1.cs
@@ -10,21 +10,32 @@ namespace Lakeside2
         private SpriteBatch _spriteBatch;
         private InputHandler input;
 
+        const int SCREEN_WIDTH = 1280;
+        const int SCREEN_HEIGHT = 720;
+        const bool FULLSCREEN = false;
+
+        const int INTERNAL_WIDTH = 320;
+        const int INTERNAL_HEIGHT = 180;
+
+        RenderTarget2D mainTarget;
+
         TileMap testMap;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         protected override void Initialize()
         {
-            _graphics.PreferredBackBufferWidth = 1280;
-            _graphics.PreferredBackBufferHeight = 720;
-            _graphics.IsFullScreen = false;
+            _graphics.PreferredBackBufferWidth = SCREEN_WIDTH;
+            _graphics.PreferredBackBufferHeight = SCREEN_HEIGHT;
+            _graphics.IsFullScreen = FULLSCREEN;
             _graphics.ApplyChanges();
+
+            mainTarget = new RenderTarget2D(GraphicsDevice, INTERNAL_WIDTH, INTERNAL_HEIGHT);
 
             input = new InputHandler();
 
@@ -50,13 +61,20 @@ namespace Lakeside2
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            // draw everything to render target
+            GraphicsDevice.SetRenderTarget(mainTarget);
             _spriteBatch.Begin();
-
+            // most drawing happens here...
             testMap.draw(_spriteBatch, Vector2.Zero);
-
             _spriteBatch.End();
+
+            // scale to screen size, draw to window
+            GraphicsDevice.SetRenderTarget(null);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(mainTarget, new Rectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), Color.White);
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
